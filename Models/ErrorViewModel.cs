64ce621c31c788aa615194ad4335f2483fa0b18b
Dorantino <seth.partnerdeals@gmail.com>
2025-12-06 12:23:31 -0400

    MVC importation

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace web_application_project_3_Dorantino.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
